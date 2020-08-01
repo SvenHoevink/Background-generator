5a96743d496a06e149e07f66cfd7870a7a3585f1
Sven Hoevink <484362@student.saxion.nl>
2020-08-01 23:09:34 +0200

    commented out the eventlisteners

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let color1 = document.querySelector('.color1');
 let color2 = document.querySelector('.color2');
 let cssOutput = document.querySelector('h3');
 
-color1.addEventListener('input', function() {
-	body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
-});
+// color1.addEventListener('input', function() {
+// 	body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
+// });
 
-color2.addEventListener('input', function() {
-	body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
-});
+// color2.addEventListener('input', function() {
+// 	body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
+// });
