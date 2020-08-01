2d5a48accfa1cad267c6b1bac76d31a863cd95e4
Sven Hoevink <svenhoevink@gmail.com>
2020-08-01 23:38:02 +0200

    finalised the background generator

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let color1 = document.querySelector('.color1');
 let color2 = document.querySelector('.color2');
 let cssOutput = document.querySelector('h3');
+let body = document.querySelector('body');
 
-color1.addEventListener('input', function() {
+const changeColor = () => {
 	body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
-});
+	cssOutput.textContent = body.style.background + ';';
+};
 
-color2.addEventListener('input', function() {
-	body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
-});
+color1.addEventListener('input', changeColor);
+color2.addEventListener('input', changeColor);
